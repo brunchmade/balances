0dc7080136d3220ede4108334ef40bbf74dde9a1
Wells Riley <wells.riley@gmail.com>
2014-03-13 17:02:34 -0700

    production env compiles landing css and js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Balances::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( landing.css landing.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
