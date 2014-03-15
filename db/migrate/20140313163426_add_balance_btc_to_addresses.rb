97a3b27082b0ad7e18f64ccaecf43f241d576747
Ian Hirschfeld <ianh7976@gmail.com>
2014-03-14 18:22:32 -0700

    updates migration file so its not dependenton CurrencyConversion already being populated

--- db/migrate/20140313163426_add_balance_btc_to_addresses.rb
+++ db/migrate/20140313163426_add_balance_btc_to_addresses.rb
@@ -1,14 +1,5 @@
 class AddBalanceBtcToAddresses < ActiveRecord::Migration
-  def up
+  def change
     add_column :addresses, :balance_btc, :decimal, precision: 18, scale: 8, default: 0
-
-    Address.find_each do |address|
-      currency = address.get_currency
-      address.update_attributes(balance_btc: currency.to_btc(address.balance))
-    end
-  end
-
-  def down
-    remove_column :addresses, :balance_btc
   end
 end
