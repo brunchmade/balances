96f640ea479f4d18a9fe7025bddfbefb3fafa621
Wells Riley <wells.riley@gmail.com>
2014-05-01 14:56:57 -0700

    [ADD] teaser page with email signup

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,12 +4,17 @@ class StaticController < ApplicationController
     if user_signed_in?
       redirect_to addresses_path
     else
-      render :landing, layout: 'landing'
+      #render :landing, layout: 'landing'
+      render :teaser, layout: false
     end
   end
 
   def home
     render :landing, layout: 'landing'
+  end
+
+  def teaser
+    render :teaser, layout: false
   end
 
   def terms_privacy
