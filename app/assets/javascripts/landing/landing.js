6661f698ee64a61c743c2795d1934c4af4d21bc4
Wells Riley <wells.riley@gmail.com>
2014-05-30 13:00:54 -0700

    [update] wip landing page

--- app/assets/javascripts/landing/landing.js
+++ app/assets/javascripts/landing/landing.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require ./wow.min
 //= require ./main
--- app/assets/javascripts/landing/main.js
+++ app/assets/javascripts/landing/main.js
@@ -1,1 +1,18 @@
 $(function(){ $(document).foundation(); });
+
+$(function() {
+    $(window).resize(function() {
+        $('.header-bg').height($(window).height() - $('.header-bg').offset().top);
+    });
+    $(window).resize();
+});
+
+new WOW().init();
+
+$(".read-more").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $("#unified-bg").offset().top
+    }, 1000);
+
+});
