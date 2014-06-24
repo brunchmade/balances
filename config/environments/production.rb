11b09be772e1c295ef088161501026be8ab2451d
Wells Riley <wells.riley@gmail.com>
2014-06-24 00:19:31 -0700

    enables Rack::Cache on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Balances::Application.configure do
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = true
 
   # Enables Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
