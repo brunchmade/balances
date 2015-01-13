67d3b2b22df48e6c097a7c55a515c55d6c7bd3d9
Wells Riley <wells.riley@gmail.com>
2015-01-12 22:10:09 -0800

    removed cloudfront asset service

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,8 @@ Balances::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['ASSET_HOST']
-  config.action_mailer.asset_host = ENV['ASSET_HOST']
+  # config.action_controller.asset_host = ENV['ASSET_HOST']
+  # config.action_mailer.asset_host = ENV['ASSET_HOST']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
