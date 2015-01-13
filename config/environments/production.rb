d42a5070a1e1589a41b3673d4a74e03c04e2b54d
Wells Riley <wells.riley@gmail.com>
2015-01-13 07:26:56 -0800

    re-added asset_host for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Balances::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = ENV['ASSET_HOST']
-  # config.action_mailer.asset_host = ENV['ASSET_HOST']
+  config.action_mailer.asset_host = ENV['ASSET_HOST']
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
